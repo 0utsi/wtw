d4017df7a9312095b13055b7afb345077f0f288d
Mateusz <mrudyk96@gmail.com>
2025-03-15 14:54:08 +0100

    feat: history page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,14 +1,10 @@
-
-
-
-
-
-
+import HistoryPage from "@/components/history/search-history";
+import SearchStatistics from "@/components/history/search-statistics";
 
 export default function Page() {
-
-
-
-
-    return
+  return (
+    <div className="flex flex-col gap-6 py-20">
+      <HistoryPage /> <SearchStatistics />
+    </div>
+  );
 }
