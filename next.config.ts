f91db498311f19c8a125fe5ab9a57c129e104d69
Mateusz <mrudyk96@gmail.com>
2025-03-16 10:39:15 +0100

    fix: config vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  reactStrictMode: true,
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
-
 };
 
 module.exports = nextConfig;
